4ab2b64231ec38091b20b9fe6c8597e50cc70952
thinkAmi <dev.thinkami+git@gmail.com>
2024-12-28 16:38:14 +0900

    fix

--- lib/hello_file_writer.rb
+++ lib/hello_file_writer.rb
@@ -6,11 +6,7 @@ module HelloFileWriter
   class Error < StandardError; end
   
   def self.write_file
-    puts Dir.pwd
-<<<<<<< HEAD
-    File.open('file_by_github_gem', 'w', 0755) { |f| f.puts 'open and write by gem.' }
-=======
-    File.open('file_by_gem', 'w', 0755) { |f| f.puts 'open and write by github gem.' }
->>>>>>> a278c1f010b4c1285379e0404924c4cf0b97395a
+    puts "Hello GitHub"
+    File.open('file_by_github_gem', 'w', 0755) { |f| f.puts 'open and write by GitHub gem' }
   end
 end
